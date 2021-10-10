be72ed8ef302c2eb0d19dc35a42eb3f915530c79
sebastian ramirez arboleda <63007415+kentica@users.noreply.github.com>
2021-10-10 13:14:42 -0500

    Punto 1 Implementacion final
    Ya tenia el punto 1 hecho y lo adjunto

---
+++ Punto1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Punto1So
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] var = new int[100];
+            array Arreglo1 = new array(var);
+
+            Arreglo1.printarray(var);
+        }
+    }
+    class array
+    {
+        
+        public array(int [] Arreglo)
+        {
+            for(int i = 0;i<100;i++)
+            {
+                Arreglo [i] = i;
+            }
+        }
+        public void printarray(int [] Arreglo)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(Arreglo [i]);
+            }                       
+        }
+
+        ~array()
+        {
+            Console.WriteLine("Se ha limpiado el array");
+        }    
+    }
+}
